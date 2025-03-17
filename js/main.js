dd3c0c911f9deb659dfe9e5bd9c53118e2f8072b
PEPENOOB16 <132952380+PEPENOOB16@users.noreply.github.com>
2025-03-17 16:00:48 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,80 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container form", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header__container a", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// navbar
+window.onscroll = function() {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper", {
+    loop: true, // Hace que el slider se repita infinitamente
+    autoplay: {
+      delay: 3000, // Cambia cada 5 segundos (5000 ms)
+      disableOnInteraction: false, // Sigue funcionando incluso si el usuario interactúa
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true, // Permite hacer clic en los puntos de paginación
+    },
+    speed: 1000, // Velocidad de la transición entre slides (1s)
+  });
+});
+
+document.querySelectorAll(".faq-item").forEach(item => {
+  item.addEventListener("click", () => {
+      item.classList.toggle("active");
+  });
+});
